89139b996bebc22372ccb5abef1307a10f0cfa7e
kurapikats <kurapikats@yaho.com>
2017-03-15 22:18:10 +0800

    add redux add higher order component for signinscreen

--- index.android.js
+++ index.android.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import {
   AppRegistry
 } from 'react-native';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import App from './components/App'
+import chatNowReducers from './reducers'
+
+let store = createStore(chatNowReducers)
 
 export default class chat extends Component {
   render() {
+    console.log('store', store.getState());
     return (
-      <App />
-    );
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
   }
 }
 
